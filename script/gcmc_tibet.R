e2168172a9a0cf208eb4626ffa9b6f5162496b6b
SpatLyu <lyu.geosocial@gmail.com>
2025-02-25 14:13:24 +0800

    update script

--- script/gcmc_tibet.R
+++ script/gcmc_tibet.R
@@ -1,11 +1,21 @@
-bio = readr::read_csv('./tibet2020.csv') |> 
+bio = readr::read_csv('./data/tibet2020.csv') |> 
   sf::st_as_sf(coords = c("x","y"), crs = 4326)
 
-spEDM::gccm(data = bio,
-            cause = "sm",
-            effect = "bio",
-            libsizes = 450,
-            E = 3)
+k = floor(nrow(bio) / 4)
+
+bio_res = spEDM::gcmc(data = bio,
+                      cause = "ndvi",
+                      effect = "bio",
+                      E = 3,
+                      k = k,
+                      r = 0,
+                      trend.rm = FALSE)
+
+g = spEDM::gccm(data = bio,
+                cause = "ndvi",
+                effect = "bio",
+                libsizes = seq(100,1600,100),
+                E = 3)
 
 spEDM::gccm(data = bio,
             cause = "pre",
