ab54280b5e197e3439a2e3b6c7c12293bad28085
SpatLyu <lyu.geosocial@gmail.com>
2025-02-27 10:06:37 +0800

    update internal functions

--- script/.internal_funs.r
+++ script/.internal_funs.r
@@ -1,3 +1,19 @@
+gcmc4lattice = \(data,lib,pred = lib,E,k = 4,tau = 1,trend.rm = FALSE) {
+  vars = names(data)[-which(names(data) == sdsfun::sf_geometry_name(data))]
+  
+  for (v in vars){
+    g = spEDM::gcmc(data = data,
+                    target = v,
+                    lib = lib,
+                    pred = pred,
+                    E = E,
+                    k = k,
+                    tau = tau,
+                    trend.rm = trend.rm)
+  }
+  return(NULL)
+}
+
 gcmc4lattice = \(data,E,k,r = 0,trend.rm = FALSE,verbose = TRUE) {
     vars = names(data)[-which(names(data) == sdsfun::sf_geometry_name(data))]
     vars = utils::combn(vars,2,simplify = FALSE)
