10ecb048385095ed42b283adf748b04ee207b6cb
SpatLyu <lyu.geosocial@gmail.com>
2025-02-28 23:04:12 +0800

    update scripts & figures

--- script/lorenz.py
+++ script/lorenz.py
@@ -33,47 +33,19 @@ for i in range(N - 1):
     y[i + 1] = y[i] + (dy * dt)
     z[i + 1] = z[i] + (dz * dt)
     
-# Plotting the figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 ax.plot(x, y, z, lw=0.45,color='#aec4ca')
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
-# ax.set_title("Lorenz Attractor")
-
-# Hide the axes
 ax.set_axis_off()
-
-# Set the figure background to transparent
 fig.patch.set_alpha(0)
-
 plt.savefig('../figure/lorenz1.png',dpi = 300,transparent=True)
 
-# Show the plot
-plt.show()
-
-# Plotting the figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 ax.plot(x, y, z, lw=0.45,color='#fabcbd')
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
-# ax.set_title("Lorenz Attractor")
-
-# Hide the axes
 ax.set_axis_off()
-
-# Set the figure background to transparent
 fig.patch.set_alpha(0)
-
 plt.savefig('../figure/lorenz2.png',dpi = 300,transparent=True)
-
-# Show the plot
-plt.show()
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -82,44 +54,28 @@ from mpl_toolkits.mplot3d import Axes3D
 m1 = pd.read_csv('../result/phase_space1.csv')
 m2 = pd.read_csv('../result/phase_space2.csv')
 
-# Plotting the figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 ax.scatter(m1["x"], m1["y"], m1["z"], lw=0.001,color='#aec4ca')
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
-# ax.set_title("Lorenz Attractor")
-
-# Hide the axes
-ax.set_axis_off()
-
-# Set the figure background to transparent
 fig.patch.set_alpha(0)
+plt.savefig('../figure/state1.jpg',dpi = 300,transparent=True)
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(m2["x"], m2["y"], m2["z"], lw=0.001, color='#fabcbd')
+fig.patch.set_alpha(0)
+plt.savefig('../figure/state2.jpg',dpi = 300,transparent=True)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(m1["x"], m1["y"], m1["z"], lw=0.001,color='#aec4ca')
+ax.set_axis_off()
+fig.patch.set_alpha(0)
 plt.savefig('../figure/phase_state1.png',dpi = 300,transparent=True)
 
-# Show the plot
-plt.show()
-
-# Plotting the figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 ax.scatter(m2["x"], m2["y"], m2["z"], lw=0.001, color='#fabcbd')
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
-# ax.set_title("Lorenz Attractor")
-
-# Hide the axes
 ax.set_axis_off()
-
-# Set the figure background to transparent
 fig.patch.set_alpha(0)
-
 plt.savefig('../figure/phase_state2.png',dpi = 300,transparent=True)
-
-# Show the plot
-plt.show()
--- script/phase_space.r
+++ script/phase_space.r
@@ -1,9 +1,59 @@
 tibetbio = readr::read_csv('./data/tibet_bio.csv') |> 
   sf::st_as_sf(coords = c("x","y"), crs = 4326)
 
+# original state space：
+library(tmap)
+library(sf)
+library(terra)
+
+bbox = sf::st_bbox(tibetbio) |> 
+  sf::st_as_sfc() |> 
+  sf::st_as_sf()
+
+elev = elevatr::get_elev_raster(bbox, z = 4, clip = "bbox") |> 
+  terra::rast()
+names(elev) = 'elevation'
+plot(elev)
+
+elev = terra::rast(tmap::land)[[4]] |> 
+  crop(vect(tibetbio),mask = FALSE)
+
+
+map1 = tm_shape(elev)+
+  tm_raster("elevation", 
+            col.scale = tm_scale_continuous(values = terrain.colors(9)),
+            col_alpha = 0.85, col.legend = tm_legend_hide()) +
+  tm_style("classic",
+           frame.doule.line = FALSE,
+           earth.boundary = FALSE) +
+  tm_shape(tibetbio) +
+  tm_dots(col = "#4F767F", fill = "#4F767F",size = "sm",
+          size.scale = tm_scale_continuous(values.scale = 0.5,
+                                           values = 1:5),
+          size.legend = tm_legend_hide())
+tmap_save(map1,'./figure/map1.jpg',dpi = 300)
+
+map2 = tm_shape(elev)+
+  tm_raster("elevation", 
+            col.scale = tm_scale_continuous(values = terrain.colors(9)),
+            col_alpha = 0.85, col.legend = tm_legend_hide()) +
+  tm_style("classic",
+           frame.doule.line = FALSE,
+           earth.boundary = FALSE) +
+  tm_shape(tibetbio) +
+  tm_symbols(col = "#A75F65", fill = "#A75F65", size = "bio",
+             size.scale = tm_scale_continuous(values.scale = 0.5,
+                                              values = 1:5,
+                                              midpoint = NA),
+             size.legend = tm_legend_hide())
+tmap_save(map2,'./figure/map2.jpg',dpi = 300)
+
+
+# embeddings:
 m1 = spEDM::embedded(tibetbio,"sm",E = 3,tau = 5,trend.rm = FALSE)
 m2 = spEDM::embedded(tibetbio,"bio",E = 3,tau = 6,trend.rm = FALSE)
 colnames(m1) = colnames(m2) = c("x","y","z")
 
 readr::write_csv(as.data.frame(m1),'./result/phase_space1.csv')
 readr::write_csv(as.data.frame(m2),'./result/phase_space2.csv')
+
