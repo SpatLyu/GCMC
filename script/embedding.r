0f4908accc6ead382fdaa767b42023b15824d5be
SpatLyu <lyu.geosocial@gmail.com>
2025-03-03 12:51:43 +0800

    update embedding srcipt

--- script/embedding.r
+++ script/embedding.r
@@ -4,20 +4,19 @@ library(tmap)
 # henan = cnmap::getMap(code = "410000", subRegion = TRUE) |> 
 #   dplyr::select(adcode,name)
 
-henan = mapchina::china |> 
-  dplyr::filter(Code_Province == "41") |> 
-  dplyr::select(Code = Code_Perfecture,Pop = Pop_2010,Area) |> 
+cn = dplyr::select(mapchina::china,
+                   Code = Code_Perfecture,
+                   Pop = Pop_2010,Area)
+henan = cn |> 
+  dplyr::filter(Code == "41") |> 
   dplyr::group_by(Code) |> 
   dplyr::summarise(geometry = sf::st_union(geometry) |> 
                        sf::st_cast("MULTIPOLYGON"),
                    Pop = sum(Pop,na.rm = TRUE),
                    Area = sum(Area,na.rm = TRUE)) |> 
   dplyr::mutate(popdensity = round(Pop / Area,0)) |> 
-  dplyr::select(Code,popdensity)
-
-mapview::mapview(henan,zcol = "Code")
-
-henan[henan$Code == "4110",]
+  dplyr::select(Code,popdensity) |> 
+  tibble::rowid_to_column(var = "rid")
 
 map1 = tm_shape(henan) + 
   tm_polygons(fill = "popdensity",fill.legend = tm_legend_hide()) +
@@ -25,13 +24,38 @@ map1 = tm_shape(henan) +
           options = opt_tm_text(just = "top",on_surface = TRUE))
 tmap_save(map1,'./figure/map1.jpg',dpi = 300)
 
-nb = spEDM:::RcppLaggedNeighbor4Lattice(sdsfun::spdep_nb(henan),3)
+# Selected spatial unit for illustrative calculation
+mapview::mapview(henan,zcol = "Code")
+henan[henan$Code == "4110",]
+nb = sdsfun::spdep_nb(henan)
+
+spunit = list()
+spunit[[1]] = nb[[10]]
+spunit[[2]] = setdiff(spEDM:::RcppLaggedNeighbor4Lattice(nb,2)[[10]],c(nb[[10]],10))
+spunit[[3]] = setdiff(spEDM:::RcppLaggedNeighbor4Lattice(nb,3)[[10]],
+                      spEDM:::RcppLaggedNeighbor4Lattice(nb,2)[[10]])
+
+henan = henan |> 
+  dplyr::mutate(
+    lagnum = dplyr::case_when(rid == 10 ~ "0",
+                              rid %in% spunit[[1]] ~ "1",
+                              rid %in% spunit[[2]] ~ "2",
+                              rid %in% spunit[[3]] ~ "3")
+  ) |> 
+  dplyr::mutate(lagnum = factor(lagnum,levels = as.character(0:3)))
+
 
 map2 = tm_shape(henan) + 
-  tm_polygons(fill = "popdensity",fill.legend = tm_legend_hide()) +
+  tm_polygons(fill = "lagnum",
+              fill.scale = tm_scale_categorical(n.max = 4,
+                                                values = c("#f89851",#"#d52f28",
+                                                           "#faf7ca",
+                                                           "#95d0b0",
+                                                           "#36bbc4")),
+              col = 'grey', lwd = 1.25) +
   tm_text("popdensity",size = 0.75, # angle = 5,
           options = opt_tm_text(just = "top",on_surface = TRUE))
-tmap_save(map1,'./figure/map1.jpg',dpi = 300)
+tmap_save(map2,'./figure/map2.jpg',dpi = 300)
 
 embeddings = spEDM::embedded(henan, target = "popdensity", E = 3, tau = 1)
 scatterplot3d::scatterplot3d(embeddings[,1:3], pch = 16, 
