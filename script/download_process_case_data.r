6a916ed4ce108455110a7d4a02312df0495c6864
SpatLyu <lyu.geosocial@gmail.com>
2025-03-05 12:58:03 +0800

    update case data & script

---
+++ script/download_process_case_data.r
@@ -0,0 +1,91 @@
+library(rgee)
+# authorize GEE permissions
+ee_clean_user_credentials()
+ee_Initialize()
+ee_Initialize(drive = T, gcs = T)
+
+library(sf)
+library(terra)
+
+roi = read_sf('./data/tibet.gpkg') |> 
+  st_bbox() |> 
+  st_as_sfc() |> 
+  sf_as_ee()
+
+# download net primary productivity
+npp = ee$ImageCollection('MODIS/061/MOD17A3HGF')$
+         filter(ee$Filter$calendarRange(2021, 2021, 'year'))$
+         select("Npp")$
+         sum()$
+         clip(roi)$
+         multiply(1e-4)$
+         rename("npp")
+ee_as_rast(
+  image = npp,
+  region = roi,
+  scale = 10000,
+  crs = 'EPSG:4326',
+  dsn = "./data/npp21.tif"
+)
+
+# download precipitation accumulation and soil moisture
+terraclimate = ee$ImageCollection('IDAHO_EPSCOR/TERRACLIMATE')$
+  filter(ee$Filter$calendarRange(2021, 2021, 'year'))
+
+pre = terraclimate$
+  select("pr")$
+  sum()$
+  toDouble()$
+  clip(roi)$
+  rename("pre")
+ee_as_rast(
+  image = pre,
+  region = roi,
+  scale = 10000,
+  crs = 'EPSG:4326',
+  dsn = "./data/pre21.tif"
+)
+
+sm = terraclimate$
+  select("soil")$
+  sum()$
+  multiply(0.1)$
+  clip(roi)$
+  rename("sm")
+ee_as_rast(
+  image = sm,
+  region = roi,
+  scale = 10000,
+  crs = 'EPSG:4326',
+  dsn = "./data/sm21.tif"
+)
+  
+# download elevation
+elev = ee$Image('USGS/SRTMGL1_003')$
+  select('elevation')$
+  clip(roi)$
+  rename("elev")
+ee_as_rast(
+  image = elev,
+  region = roi,
+  scale = 10000,
+  crs = 'EPSG:4326',
+  dsn = "./data/elev.tif"
+)
+
+# process temperature
+tibet = vect('./data/tibet.gpkg')
+npp = rast(paste0("./data/",c("pre21","sm21","elev","npp21"),".tif"))
+tem = rast('./data/tmp_2021.nc') |> 
+  app("mean",na.rm = TRUE, cores = 8) |> 
+  app(\(.x) .x * 0.1) |> 
+  aggregate(fact = 10, fun = "mean", na.rm = TRUE) |> 
+  crop(tibet) |> 
+  resample(npp, method = "bilinear", threads = TRUE)
+names(tem) = "tem"
+writeRaster(tem,'./data/tem21.tif',overwrite = TRUE)
+
+# clip data with the tibet polygon
+npp = c(tem,npp) |> 
+  crop(tibet, mask = TRUE)
+writeRaster(npp,'./data/npp.tif')
