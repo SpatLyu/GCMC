396b984197dadc051002972635f664da157c9e12
SpatLyu <lyu.geosocial@gmail.com>
2025-03-14 20:18:13 +0800

    update case1 script

--- script/case1.r
+++ script/case1.r
@@ -15,13 +15,32 @@ columbus = sf::read_sf(system.file("case/columbus.gpkg", package="spEDM"))
 #------------------------------------------------------------------------------#
 
 # housing value and crime (residential burglaries and vehicle thefts)
-g1 = gcmc(data = columbus,cause = "hoval",effect = "crime",E = 3,k = 20)
+g1 = gcmc(data = columbus,cause = "hoval",effect = "crime",E = c(6,8),k = 20)
 g1
 
 # household income and crime (residential burglaries and vehicle thefts)
-g2 = gcmc(data = columbus,cause = "inc",effect = "crime",E = 3,k = 20)
+g2 = gcmc(data = columbus,cause = "inc",effect = "crime",E = c(5,8),k = 20)
 g2
+
+# housing value and household income
+g3 = gcmc(data = columbus,cause = "hoval",effect = "inc",E = c(6,5),k = 20)
+g3
 
 #------------------------------------------------------------------------------#
 #------    Causality by Geographical Convergent Cross Mapping (GCCM)     ------#
 #------------------------------------------------------------------------------#
+
+# housing value and crime (residential burglaries and vehicle thefts)
+g1 = gccm(data = columbus,cause = "hoval",effect = "crime",
+          libsizes = seq(5,45,5), E = c(6,8), k = c(6,10))
+g1
+
+# household income and crime (residential burglaries and vehicle thefts)
+g2 = gccm(data = columbus,cause = "inc",effect = "crime",
+          libsizes = seq(5,45,5), E = c(5,8), k = c(3,10))
+g2
+
+# housing value and household income
+g3 = gccm(data = columbus,cause = "hoval",effect = "inc",
+          libsizes = seq(5,45,5), E = c(6,5), k = c(6,10))
+g3
