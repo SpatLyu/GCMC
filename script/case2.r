06974b8b0eb4b4e8617c52106fc36630471cb0cf
SpatLyu <lyu.geosocial@gmail.com>
2025-03-20 11:31:59 +0800

    update case2 script

--- script/case2.r
+++ script/case2.r
@@ -11,7 +11,9 @@ library(spEDM)
 
 popd_nb = spdep::read.gal(system.file("case/popdensity_nb.gal",package = "spEDM"))
 popdensity = readr::read_csv(system.file("case/popdensity.csv",package = "spEDM"))
-popd_sf = sf::st_as_sf(popdensity, coords = c("x","y"), crs = 4326)
+popd_sf = popdensity |> 
+  sf::st_as_sf(coords = c("x","y"), crs = 4326) |> 
+  dplyr::select(popd = popdensity,elev,tem)
 popd_sf
 
 #------------------------------------------------------------------------------#
@@ -19,12 +21,12 @@ popd_sf
 #------------------------------------------------------------------------------#
 
 # temperature and population density
-g1 = gcmc(data = popd_sf, cause = "tem", effect = "popdensity",
+g1 = gcmc(data = popd_sf, cause = "tem", effect = "popd",
           E = c(2,5), k = 160, nb = popd_nb, trend.rm = TRUE)
 g1
 
 # elevation and population density
-g2 = gcmc(data = popd_sf, cause = "elev", effect = "popdensity",
+g2 = gcmc(data = popd_sf, cause = "elev", effect = "popd",
           E = c(1,5), k = 150, nb = popd_nb, trend.rm = TRUE)
 g2
 
@@ -41,12 +43,12 @@ readr::write_rds(gcmc_case2,'./result/case/gcmc_case2.rds')
 #------------------------------------------------------------------------------#
 
 # temperature and population density
-g1 = gccm(data = popd_sf, cause = "tem", effect = "popdensity",
+g1 = gccm(data = popd_sf, cause = "tem", effect = "popd",
           libsizes = seq(10, 2800, by = 100),E = c(2,5),k = 6,nb = popd_nb)
 g1
 
 # elevation and population density
-g2 = gccm(data = popd_sf, cause = "elev", effect = "popdensity",
+g2 = gccm(data = popd_sf, cause = "elev", effect = "popd",
           libsizes = seq(10, 2800, by = 100),E = c(1,5),k = 6,nb = popd_nb)
 g2
 
@@ -62,7 +64,7 @@ readr::write_rds(gccm_case2,'./result/case/gccm_case2.rds')
 #------        Correlation by Pearson Correlation Coefficient(PCC)       ------#
 #------------------------------------------------------------------------------#
 
-popdf = sf::st_drop_geometry(dplyr::select(popd_sf,popdensity,elev,tem))
+popdf = sf::st_drop_geometry(popd_sf)
 pcc = psych::corr.test(popdf)
 pcc
 readr::write_rds(pcc,'./result/case/pcc_case2.rds')
@@ -72,8 +74,8 @@ readr::write_rds(pcc,'./result/case/pcc_case2.rds')
 #------------------------------------------------------------------------------#
 
 source('./script/ssh_q.r')
-q1 = ssh_q(data = popdf, cause = "tem", effect = "popdensity")
-q2 = ssh_q(data = popdf, cause = "elev", effect = "popdensity")
+q1 = ssh_q(data = popdf, cause = "tem", effect = "popd")
+q2 = ssh_q(data = popdf, cause = "elev", effect = "popd")
 q3 = ssh_q(data = popdf, cause = "elev", effect = "tem")
 qv = do.call(rbind,list(q1,q2,q3))
 qv
