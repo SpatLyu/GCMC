3ec92512b3b97040ca1c55531df871ebeb43bb5a
SpatLyu <lyu.geosocial@gmail.com>
2025-06-26 01:26:43 +0800

    update figures

--- manuscript/writing_helpers.r
+++ manuscript/writing_helpers.r
@@ -3,7 +3,7 @@
   pdftools::pdf_convert(pdf = pdfname, filenames = jpgname, dpi = dpi)
 }
 
-1:6 |> 
+1:7 |> 
   purrr::walk(\(.x) {
     .pdf2jpg(paste0('./figure/figure',.x,'.pdf'),
              paste0('./manuscript/figure/figure',.x,'.jpg'))
