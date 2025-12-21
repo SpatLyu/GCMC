7e0650c69c7e8fda86cd8de0624cd7010eb25baa
SpatLyu <lyu.geosocial@gmail.com>
2025-12-21 23:43:04 +0800

    update npp case script

--- gcmc_data_codes/Case of net primary productivity study/Case of net primary productivity study.r
+++ gcmc_data_codes/Case of net primary productivity study/Case of net primary productivity study.r
@@ -8,10 +8,6 @@
 
 library(spEDM)
 npp = terra::rast('./Case of net primary productivity study/npp.tif')
-# To save the computation time, we will aggregate the data by 3 times and 
-# select 1500 non-NA pixels to predict:
-npp = terra::aggregate(npp, fact = 3, na.rm = TRUE)
-npp = npp[[c("npp","pre","tem")]]
 npp
 
 terra::global(npp,"isNA")
@@ -30,25 +26,25 @@ predindice = nnaindice[indices,]
 #------            Determining minimum embedding dimension              ------#
 #-----------------------------------------------------------------------------#
 
-spEDM::fnn(npp, "npp", E = 1:25, lib = predindice, pred = predindice,
-           eps = stats::sd(terra::values(npp[["npp"]]),na.rm = TRUE) / 10)
+spEDM::fnn(npp, "npp", E = 1:25,
+           eps = stats::sd(terra::values(npp[["npp"]]),na.rm = TRUE))
 
 #------------------------------------------------------------------------------#
 #------    Causation by Geographical Cross Mapping Cardinality (GCMC)    ------#
 #------------------------------------------------------------------------------#
 
-ceiling(sqrt(18 * 1500))
+ceiling(sqrt(5 * dim(nnaindice)[1]))
 
 # precipitation and npp
-g1 = gcmc(npp, "pre", "npp", E = 18, k = 165, lib = predindice, pred = predindice)
+g1 = gcmc(npp, "pre", "npp", E = 5, k = 144)
 g1
 
 # temperature and npp
-g2 = gcmc(npp, "tem", "npp", E = 18, k = 165, lib = predindice, pred = predindice)
+g2 = gcmc(npp, "tem", "npp", E = 5, k = 144)
 g2
 
 # precipitation and temperature
-g3 = gcmc(npp, "pre", "tem", E = 18, k = 165, lib = predindice, pred = predindice)
+g3 = gcmc(npp, "pre", "tem", E = 5, k = 144)
 g3
 
 gcmc_case3 = list(g1,g2,g3)
@@ -59,15 +55,15 @@ readr::write_rds(gcmc_case3,'./Case of net primary productivity study/gcmc_case3
 #------------------------------------------------------------------------------#
 
 # precipitation and npp
-g1 = gccm(npp, "pre", "npp", E = 18, k = 20, lib = predindice, pred = predindice)
+g1 = gccm(npp, "pre", "npp", E = 5, k = 7)
 g1
 
 # temperature and npp
-g2 = gccm(npp, "tem", "npp", E = 18, k = 20, lib = predindice, pred = predindice)
+g2 = gccm(npp, "tem", "npp", E = 5, k = 7)
 g2
 
 # precipitation and temperature
-g3 = gccm(npp, "pre", "tem", E = 18, k = 20, lib = predindice, pred = predindice)
+g3 = gccm(npp, "pre", "tem", E = 5, k = 7)
 g3
 
 gccm_case3 = list(g1,g2,g3)
@@ -77,9 +73,7 @@ readr::write_rds(gccm_case3,'./Case of net primary productivity study/gccm_case3
 #------       Correlation by Pearson Correlation Coefficient (PCC)       ------#
 #------------------------------------------------------------------------------#
 
-npp.df = dplyr::filter(npp[terra::cellFromRowCol(npp,predindice[,1],predindice[,2])],
-                       dplyr::if_all(dplyr::everything(),
-                                     \(.x) !is.na(.x)))
+npp.df = terra::as.data.frame(npp,na.rm = TRUE)
 pcc = psych::corr.test(npp.df)
 pcc
 readr::write_rds(pcc,'./Case of net primary productivity study/pcc_case3.rds')
