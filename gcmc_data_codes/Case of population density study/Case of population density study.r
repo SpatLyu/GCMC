ba75f2fd50aff7c3868ca323fdf777964585fb78
SpatLyu <lyu.geosocial@gmail.com>
2025-12-22 09:52:40 +0800

    update popd case study

--- gcmc_data_codes/Case of population density study/Case of population density study.r
+++ gcmc_data_codes/Case of population density study/Case of population density study.r
@@ -47,15 +47,15 @@ readr::write_rds(gcmc_case2,'./Case of population density study/gcmc_case2.rds')
 #------------------------------------------------------------------------------#
 
 # temperature and population density
-g1 = gccm(popd_sf, "tem", "popd", E = 10, k = 12, nb = popd_nb)
+g1 = gccm(popd_sf, "tem", "popd", E = 11, k = 13, nb = popd_nb)
 g1
 
 # elevation and population density
-g2 = gccm(popd_sf, "elev", "popd", E = 10, k = 12, nb = popd_nb)
+g2 = gccm(popd_sf, "elev", "popd", E = 11, k = 13, nb = popd_nb)
 g2
 
 # elevation and temperature
-g3 = gccm(popd_sf, "elev", "tem", E = 10, k = 12, nb = popd_nb)
+g3 = gccm(popd_sf, "elev", "tem", E = 11, k = 13, nb = popd_nb)
 g3
 
 gccm_case2 = list(g1,g2,g3)
