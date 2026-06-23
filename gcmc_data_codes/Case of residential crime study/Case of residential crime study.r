3ea1126680460630f2a1fa560e50890a519258ae
SpatLyu <lyu.geosocial@gmail.com>
2026-06-24 00:00:01 +0800

    unify code style

--- gcmc_data_codes/Case of residential crime study/Case of residential crime study.r
+++ gcmc_data_codes/Case of residential crime study/Case of residential crime study.r
@@ -35,7 +35,7 @@ g3 = gcmc(columbus, "hoval", "inc", E = 7, k = 19)
 g3
 
 gcmc_case1 = list(g1,g2,g3)
-readr::write_rds(gcmc_case1,'./Case of residential crime study/gcmc_case1.rds')
+readr::write_rds(gcmc_case1,"./Case of residential crime study/gcmc_case1.rds")
 
 #------------------------------------------------------------------------------#
 #------    Causation by Geographical Convergent Cross Mapping (GCCM)     ------#
@@ -54,7 +54,7 @@ g3 = gccm(columbus, "hoval", "inc", E = 7, k = 9)
 g3
 
 gccm_case1 = list(g1,g2,g3)
-readr::write_rds(gccm_case1,'./Case of residential crime study/gccm_case1.rds')
+readr::write_rds(gccm_case1,"./Case of residential crime study/gccm_case1.rds")
 
 #------------------------------------------------------------------------------#
 #------       Correlation by Pearson Correlation Coefficient (PCC)       ------#
@@ -63,35 +63,35 @@ readr::write_rds(gccm_case1,'./Case of residential crime study/gccm_case1.rds')
 columdf = sf::st_drop_geometry(dplyr::select(columbus,c(hoval,inc,crime)))
 pcc = psych::corr.test(columdf)
 pcc
-readr::write_rds(pcc,'./Case of residential crime study/pcc_case1.rds')
+readr::write_rds(pcc,"./Case of residential crime study/pcc_case1.rds")
 
 #-----------------------------------------------------------------------------#
 #------                  Causation by Direct LiNGAM                     ------#
 #-----------------------------------------------------------------------------#
 
-source('./Utils/directlingam_cf.r')
+source("./Utils/directlingam_cf.r")
 
 directlingam = run_directlingam(columdf)
-readr::write_rds(directlingam,'./Case of residential crime study/directlingam_case1.rds')
+readr::write_rds(directlingam,"./Case of residential crime study/directlingam_case1.rds")
 
 #------------------------------------------------------------------------------#
 #------            Association by Geographical Detector (GD)             ------#
 #------------------------------------------------------------------------------#
 
-source('./Utils/ssh_q.r')
+source("./Utils/ssh_q.r")
 
 q1 = ssh_q(data = columdf,cause = "hoval",effect = "crime")
 q2 = ssh_q(data = columdf,cause = "inc",effect = "crime")
 q3 = ssh_q(data = columdf,cause = "hoval",effect = "inc")
 qv = do.call(rbind,list(q1,q2,q3))
 qv
-readr::write_rds(qv,'./Case of residential crime study/gd_case1.rds')
+readr::write_rds(qv,"./Case of residential crime study/gd_case1.rds")
 
 #------------------------------------------------------------------------------#
 #------                   Handling the case results                      ------#
 #------------------------------------------------------------------------------#
 
-source('./Utils/process_results.r')
+source("./Utils/process_results.r")
 
 case1 = list(
   gcmc = readr::read_rds("./Case of residential crime study/gcmc_case1.rds") |> 
